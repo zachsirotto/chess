78a1d47998c7b68eca33c8dd5f84ce3d9dc2ef3f
Zach-Sirotto <zach.sirotto@ibm.com>
2020-12-29 20:38:34 -0500

    chore: 🔧 adds install_requires to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/zachsirotto/chess",
     packages=setuptools.find_packages(),
+    install_requires=["PyQt5", "chess"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
