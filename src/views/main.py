f809fa651b5b8bec9ea931bbb480546989e0e113
Zach-Sirotto <zach.sirotto@ibm.com>
2020-12-30 16:33:17 -0500

    chore: 🔧 idk what i'm doing

--- src/views/main.py
+++ src/views/main.py
@@ -1,7 +1,7 @@
 import chess
 import chess.svg
 from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import QMainWindow, QGridLayout, QWidget  # , QTabWidget
+from PyQt5.QtWidgets import QMainWindow, QGridLayout  # , QWidget
 from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QCursor
 from .square import Square
@@ -39,19 +39,17 @@ class MainWindow(QMainWindow):
         # bt_h = dimensions.height() / 8 - dimensions.height() / 26
         layout = QGridLayout()
         # layout.setGeometry(QRect(svgx, svgY, cbSize, cbSize))
-        # layout.setContentsMargins(0, 0, 0, 0)
-        layout.setHorizontalSpacing(0)
-        layout.setVerticalSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        # layout.setHorizontalSpacing(0)
+        # layout.setVerticalSpacing(0)
         for i in range(8):
-            # layout.setRowMinimumHeight(i, squareSize)
+            # layout.setRowMinimumHeight(i, self.squareSize)
             for j in range(8):
                 self.squares[i][j] = Square(i * 8 + j, self.squareSize)
                 # self.squares[i][j].setGeometry(bt_w * j, bt_h * i, bt_w, bt_h)
                 layout.addWidget(self.squares[i][j], i, j)
         self.boardWidget.setLayout(layout)
-        # windowLayout = QVBoxLayout()
-        # windowLayout.addWidget(self.groupBox)
-        # self.setLayout(windowLayout)
 
     def paintEvent(self, event):
         self.boardWidget.load(chess.svg.board(
