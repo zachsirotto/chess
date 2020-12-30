9dde5d54efea2443bef9b49836a92543cb7d711c
Zach-Sirotto <zach.sirotto@ibm.com>
2020-12-30 14:19:39 -0500

    chore: 🔧 commits latest

--- src/views/main.py
+++ src/views/main.py
@@ -6,9 +6,15 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
 from .square import Square
 
+svgX = 50                          # top left x-pos of chessboard
+svgY = 50                          # top left y-pos of chessboard
+cbSize = 600                       # size of chessboard
+margin = 0.05 * cbSize
+#  if self.coordinates == True else 0
+squareSize = (cbSize - 2 * margin) / 8.0
+
 
 class MainWindow(QMainWindow):
-
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.squares = [[0] * 8] * 8
@@ -16,26 +22,26 @@ class MainWindow(QMainWindow):
 
         self.board = chess.Board()
 
-        squares = self.board.attacks(chess.E4)
-
         # self.mainWidget = QTabWidget(self)
-        self.boardWidget = QSvgWidget()
-        self.grid = QWidget(self)
-        self.grid.move(1, 1)
+        self.boardWidget = QSvgWidget(parent=self)
+        self.grid = QWidget(parent=self)
+        # self.grid.move(1, 1)
         # self.groupBox = QGroupBox('grid')
         self.createGrid()
 
-        self.boardWidget.load(bytearray(chess.svg.board(
-            self.board, squares=squares, size=350), 'utf8'))
+        self.coordinates = True
 
-        self.boardWidget.setGeometry(50, 50, 759, 668)
+        self.boardWidget.load(chess.svg.board(
+            self.board, size=350).encode('utf8'))
+
+        # self.boardWidget.setGeometry(100, 100, 759, 668)
+
+        # self.setGeometry(300, 300, 800, 800)
+        # self.grid.setGeometry(50, 50, cbSize, cbSize)
+        self.boardWidget.setGeometry(svgX, svgY, cbSize, cbSize)
+        # see chess.svg.py line 129
+
         self.boardWidget.show()
-
-        # label = QLabel("chess window")
-
-        # # The `Qt` namespace has a lot of attributes to customise
-        # # widgets. See: http://doc.qt.io/qt-5/qt.html
-        # label.setAlignment(Qt.AlignCenter)
 
         # self.mainWidget.addTab(self.boardWidget, 'board')
         # self.mainWidget.addTab(self.grid, 'grid')
@@ -46,30 +52,24 @@ class MainWindow(QMainWindow):
 
     def createGrid(self):
         dimensions = self.geometry()
-        bt_w = dimensions.width() / 8 - dimensions.width() / 26
-        bt_h = dimensions.height() / 8 - dimensions.height() / 26
+        # bt_w = dimensions.width() / 8 - dimensions.width() / 26
+        # bt_h = dimensions.height() / 8 - dimensions.height() / 26
         layout = QGridLayout()
+        layout.setGeometry(50, 50, cbSize, cbSize)
+        # layout.setContentsMargins(0, 0, 0, 0)
         layout.setHorizontalSpacing(0)
         layout.setVerticalSpacing(0)
         for i in range(8):
+            # layout.setRowMinimumHeight(i, squareSize)
             for j in range(8):
-                self.squares[i][j] = Square(i * 8 + j)
-                self.squares[i][j].setGeometry(bt_w * j, bt_h * i, bt_w, bt_h)
+                self.squares[i][j] = Square(i * 8 + j, squareSize)
+                # self.squares[i][j].setGeometry(bt_w * j, bt_h * i, bt_w, bt_h)
                 layout.addWidget(self.squares[i][j], i, j)
         self.boardWidget.setLayout(layout)
         # windowLayout = QVBoxLayout()
         # windowLayout.addWidget(self.groupBox)
         # self.setLayout(windowLayout)
 
-    def mouse_clicked(self, QMouseEvent):
-        # https://doc.qt.io/qt-5/qmouseevent.html
-        pos = QMouseEvent.pos()
-        # https://doc.qt.io/qt-5/qrect.html
-        # dimensions = self.geometry()
-        # x = (pos.x() - (dimensions.width() / 8 / 4))
-        # y = (pos.y() - (dimensions.height() / 8 / 4))
-        # x = (dimensions.width() / 8) % pos.x()
-        # y = (dimensions.height() / 8) % pos.y()
-        # print(x, y)
-        # print(dimensions.width() / 8 - dimensions.width() / 8 / 3)
-        # print(pos)
+    def paintEvent(self, event):
+        self.boardWidget.load(chess.svg.board(
+            self.board, size=cbSize, coordinates=self.coordinates).encode("utf8"))
--- src/views/square.py
+++ src/views/square.py
@@ -4,13 +4,16 @@ from PyQt5.Qt import Qt
 
 
 class Square(QPushButton):
-    def __init__(self, pos):
-        super(Square, self).__init__('', flat=True)
+    def __init__(self, pos, sz):
+        super(Square, self).__init__('')  # , flat=True)
         self.pos = pos
-        # self.resize(40, 12)
+        self.setMinimumWidth(sz)
+        self.setMinimumHeight(sz)
+        # self.resize(sz, sz)
+        # self.setGeometry(0, 0, sz, sz)
         self.clicked.connect(self.mouse_clicked)
-        palette = self.palette()
-        palette.setColor(QPalette.Inactive, QPalette.Window, Qt.transparent)
+        # palette = self.palette()
+        # palette.setColor(QPalette.Inactive, QPalette.Window, Qt.transparent)
 
     def mouse_clicked(self):
         print(str(self.pos) + " clicked")
