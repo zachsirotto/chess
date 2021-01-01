a79f98ef6b659f6b561f149771697848735f1c86
Zach-Sirotto <zach.sirotto@ibm.com>
2021-01-01 18:06:18 -0500

    feat: ✨ adds checking, lastmove, and more

--- src/views/main.py
+++ src/views/main.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui import QCursor
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.defaultSize = 500                  # size of chessboard
+        self.defaultSize = 500
         self.squares = [[0] * 8] * 8
         self.setWindowTitle("chess")
         self.board = chess.Board()
@@ -18,6 +18,8 @@ class MainWindow(QMainWindow):
         self.currentPlayer = chess.WHITE
         self.sqSelected = None
         self.pieceSelected = None
+        self.lastMove = None
+        self.checkSquares = []
         self.boardWidget.load(chess.svg.board(
             self.board, size=self.cbSize).encode('utf8'))
         self.boardWidget.setGeometry(0, 0, self.defaultSize, self.defaultSize)
@@ -34,6 +36,16 @@ class MainWindow(QMainWindow):
         cbWidth, cbHeight = self.cbSize()
         return floor(1/24 * cbWidth), floor(1/24 * cbHeight)
 
+    def getSquareAndPiece(self, x, y):
+        square = chess.square(x, y)
+        piece = self.board.piece_at(square)
+        return square, piece
+
+    def setSquareAndPiece(self, square):
+        self.sqSelected = square
+        self.pieceSelected = self.board.piece_at(
+            self.sqSelected)
+
     def squareSize(self):
         cbWidth, cbHeight = self.cbSize()
         marginX, marginY = self.marginSize()
@@ -41,9 +53,18 @@ class MainWindow(QMainWindow):
         squareHeight = (cbHeight - (2 * marginY)) / 8.0
         return squareWidth, squareHeight
 
+    def getRankAndFile(self, event):
+        file = int((event.x() - marginX) / sqWidth)
+        rank = 7 - int((event.y() - marginY) / sqHeight)
+        return file, rank
+
     def paintEvent(self, event):
         self.boardWidget.load(chess.svg.board(
-            self.board, size=self.cbSize).encode("utf8"))
+            self.board,
+            size=self.cbSize,
+            lastmove=self.lastMove,
+            check=self.checkSquares
+        ).encode("utf8"))
 
     def mousePressEvent(self, event):
         cbWidth, cbHeight = self.cbSize()
@@ -51,26 +72,36 @@ class MainWindow(QMainWindow):
         marginX, marginY = self.marginSize()
         if event.buttons() == Qt.LeftButton:
             if marginX <= event.x() <= cbWidth - marginX and marginY <= event.y() <= cbHeight - marginY:
-                x = int((event.x() - marginX) / sqWidth)
-                y = 7 - int((event.y() - marginY) / sqHeight)
+                file, rank = self.getRankAndFile(self, event)
                 # chess.sqare.mirror() if white is on top
                 # if sq already selected, make a move
                 if self.sqSelected:
-                    print("attempt to make move")
-                    self.board.push(chess.Move(
+                    # reselect piece if necessary
+                    square, piece = self.getSquareAndPiece(file, rank)
+                    if piece and piece.color == self.currentPlayer:
+                        self.setSquareAndPiece(square)
+                        return
+                    move = chess.Move(
                         from_square=self.sqSelected,
-                        to_square=chess.square(x, y)  # ,
+                        to_square=square  # ,
                         # promotion=self.pieceSelected
-                    ))
-                    self.sqSelected, pieceSelected = None, None
-                    self.changeTurns()
-                    self.update()
+                    )
+                    # if move is legal
+                    if move in self.board.legal_moves:
+                        self.board.push(move)
+                        self.sqSelected, pieceSelected = None, None
+                        self.changeTurns()
+                        self.lastMove = move
+                        # get check squares if move puts king in check
+                        if self.board.is_check():
+                            self.checkSquares = self.board.checkers()
+                        else:
+                            self.checkSquares = []
+                        self.update()
                 # sq is not selected yet
                 else:
-                    # get square number
-                    self.sqSelected = chess.square(x, y)
-                    # if current player's piece is not on square, do not select piece
-                    # if self.board.piece_at(square).belongsToCurrentPlayer():
-                    # find and select square
-                    self.pieceSelected = self.board.piece_at(self.sqSelected)
-                print(x, y)
+                    square, piece = self.getSquareAndPiece(file, rank)
+                    # only select current player's pieces
+                    if piece and piece.color == self.currentPlayer:
+                        self.setSquareAndPiece(square)
+                print(file, rank)
